91165e04a31aa66d1c3108a7c0f371c0cb1bcd8e
Seyi <soluwaleimu@atlassian.com>
2022-12-09 22:21:08 -0800

    added firebase to the project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFyp5pkr4sF_fUC9U868pnqG44UQMHLpw",
+  authDomain: "accountant-ability.firebaseapp.com",
+  projectId: "accountant-ability",
+  storageBucket: "accountant-ability.appspot.com",
+  messagingSenderId: "994751745152",
+  appId: "1:994751745152:web:927c69f504f03b8b8d2a65",
+};
+
+const app = initializeApp(firebaseConfig);
